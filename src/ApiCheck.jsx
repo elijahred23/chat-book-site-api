4f55b7c66b7ca0a1c3268b5f0b46c159ad9d0055
elijahred23 <elijahred23@gmail.com>
2024-02-12 21:28:06 -0600

    Eli-4: Added api with express and added api check request

--- src/ApiCheck.jsx
+++ src/ApiCheck.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const baseURL = 'http://localhost:3000';
 
 
 export default function ApiCheck() {
+    const [apiMessage, setApiMessage] = useState('');
+    const [success, setSuccess] = useState(false);
 
+    const getMessageFromAPI = async () => {
+        await fetch(`${baseURL}/`).then(res => res.json()).then(res => {
+            setSuccess(true);
+            let message = res?.message ?? 'MESSAGE NOT SENT AS RESPONSE';
+            setApiMessage(message);
+        }).catch(error => {
+            setSuccess(false);
+            setApiMessage(JSON.stringify(error));
+        })
+    }
+
+    useEffect(() => {
+        getMessageFromAPI();
+    }, []);
 
     return (<><div>
         <h2>API Check</h2>
+        <p style={{ color: success ? 'green' : 'red' }}>{apiMessage}</p>
     </div></>)
 }
