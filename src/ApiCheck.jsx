1d9bd3974ccad60372afd375154cc8173fe91ff5
elijahred23 <elijahred23@gmail.com>
2024-02-12 21:09:26 -0600

    Eli-3: Added api check page that I'll utilize pretty soon

---
+++ src/ApiCheck.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+
+export default function ApiCheck() {
+
+
+    return (<><div>
+        <h2>API Check</h2>
+    </div></>)
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
 import ChatBookApp from './ChatBookApp';
+import ApiCheck from './ApiCheck';
 
 
 
@@ -13,11 +14,13 @@ function App(){
     <BrowserRouter>
       <h1>Eli GPT</h1>
       <div className='nav-links'>
-        <NavLink to='/chatbook'>Chat Book</NavLink>
+        <NavLink to='/chatBook'>Chat Book</NavLink>
+        <NavLink to='/apiCheck'>API Check</NavLink>
       </div>
       <div className="content">
         <Routes>
-          <Route path="/chatbook" element={<ChatBookApp />} />
+          <Route path="/chatBook" element={<ChatBookApp />} />
+          <Route path="/apiCheck" element={<ApiCheck />} />
         </Routes>
       </div>
     </BrowserRouter>   
