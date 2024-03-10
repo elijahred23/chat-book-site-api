ac4996b6db7a2218e637937fae8958b3e234a8e1
elijahred23 <elijahred23@gmail.com>
2024-03-09 21:54:28 -0600

    Eli-27: Updated vite.config.js removed buggy code

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,5 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3006, // you can replace this port with any port
-    proxy:{
-      target: 'http://localhost:3005',
-      changeOrigin: true,
-      rewritePath: path=>path.replace(/^\/api/, ''),
-    }
   }
 })
