83ddee4aac189768949c578ebcc65c662161f0cb
elijahred23 <52219513+elijahred23@users.noreply.github.com>
2025-06-07 15:07:46 -0500

    V-44: Made lang english for fetch youtube transcript

--- api/youtube.js
+++ api/youtube.js
@@ -24,6 +24,7 @@ export async function fetchTranscript(urlOrId) {
 
   try {
     const transcript = await fetchYoutubeTranscript(videoId, {
+      lang: 'en',
       userAgent:
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     });
