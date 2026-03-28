90b2e45f790bffdb9bed2549856eea66df4643a1
elijahred23 <elijahred23@gmail.com>
2026-03-28 15:12:50 -0500

    V-257: Added python to coding solutions

--- src/CodingProblems.jsx
+++ src/CodingProblems.jsx
@@ -5,6 +5,7 @@ import "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-python";
 import "prismjs/themes/prism.css";
 import ActionButtons from "./ui/ActionButtons.jsx";
 import { useFlyout } from "./context/FlyoutContext";
