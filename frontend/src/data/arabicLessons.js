6c961d6a940b54c33c36cb9fb3ea0a2495eb1f29
elijahred23 <elijahred23@gmail.com>
2026-09-19 10:50:11 -0500

    Add Arabic daily prayer names lesson

--- frontend/src/data/arabicLessons.js
+++ frontend/src/data/arabicLessons.js
@@ -1,5 +1,6 @@
 import prayerWordsActions from "../arabic_lessons/prayer-words-actions.json";
 import alFatihah from "../arabic_lessons/al-fatihah.json";
 import afterPrayerRemembrance from "../arabic_lessons/after-prayer-remembrance.json";
+import fiveDailyPrayerNames from "../arabic_lessons/five-daily-prayer-names.json";
 
-export const ARABIC_LESSONS = [prayerWordsActions, alFatihah, afterPrayerRemembrance];
+export const ARABIC_LESSONS = [fiveDailyPrayerNames, prayerWordsActions, alFatihah, afterPrayerRemembrance];
