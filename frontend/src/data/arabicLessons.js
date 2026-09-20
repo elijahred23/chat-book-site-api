c1b2fa2e412f94b4d66c3cc3c034ebfe4f51c535
elijahred23 <elijahred23@gmail.com>
2026-09-19 22:24:31 -0500

    Added prayers for arabic tutor

--- frontend/src/data/arabicLessons.js
+++ frontend/src/data/arabicLessons.js
@@ -2,5 +2,10 @@ import prayerWordsActions from "../arabic_lessons/prayer-words-actions.json";
 import alFatihah from "../arabic_lessons/al-fatihah.json";
 import afterPrayerRemembrance from "../arabic_lessons/after-prayer-remembrance.json";
 import fiveDailyPrayerNames from "../arabic_lessons/five-daily-prayer-names.json";
+import fajrPrayerRecitation from "../arabic_lessons/fajr-prayer-recitation.json";
+import dhuhrPrayerRecitation from "../arabic_lessons/dhuhr-prayer-recitation.json";
+import asrPrayerRecitation from "../arabic_lessons/asr-prayer-recitation.json";
+import maghribPrayerRecitation from "../arabic_lessons/maghrib-prayer-recitation.json";
+import ishaPrayerRecitation from "../arabic_lessons/isha-prayer-recitation.json";
 
-export const ARABIC_LESSONS = [fiveDailyPrayerNames, prayerWordsActions, alFatihah, afterPrayerRemembrance];
+export const ARABIC_LESSONS = [fajrPrayerRecitation, dhuhrPrayerRecitation, asrPrayerRecitation, maghribPrayerRecitation, ishaPrayerRecitation, fiveDailyPrayerNames, prayerWordsActions, alFatihah, afterPrayerRemembrance];
